b5ce83a886d42ebce4f656cda1f8cefcabca2606
Classy <69457423+Classy08@users.noreply.github.com>
2022-11-25 19:27:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 require("v8-compile-cache");
 
-const { app, BrowserWindow, globalShortcut, autoUpdater } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
 const Store = require('electron-store');
+const { autoUpdater } = require("electron-updater")
+const { dialog } = require('electron')
+
 
 Store.initRenderer();
 
@@ -37,6 +40,7 @@ const createWindow  = () => {
     globalShortcut.register('Escape', () => win.webContents.executeJavaScript('document.exitPointerLock()', true));
     globalShortcut.register('F7', () => win.webContents.toggleDevTools());
 
+    checkForUpdates();
 
     win.loadURL('https://venge.io/')
 }
@@ -55,3 +59,52 @@ app.on("window-all-closed", () => {
       app.quit();
     }
 });
+
+let updater
+autoUpdater.autoDownload = false
+
+autoUpdater.on('error', (error) => {
+  dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
+})
+
+autoUpdater.on('update-available', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Found Updates',
+    message: 'Found updates, do you want update now?',
+    buttons: ['Sure', 'No']
+  }).then((buttonIndex) => {
+    if (buttonIndex === 0) {
+      autoUpdater.downloadUpdate()
+    }
+    else {
+      updater.enabled = true
+      updater = null
+    }
+  })
+})
+
+autoUpdater.on('update-not-available', () => {
+  dialog.showMessageBox({
+    title: 'No Updates',
+    message: 'Current version is up-to-date.'
+  })
+  updater.enabled = true
+  updater = null
+})
+
+autoUpdater.on('update-downloaded', () => {
+  dialog.showMessageBox({
+    title: 'Install Updates',
+    message: 'Updates downloaded, application will be quit for update...'
+  }).then(() => {
+    setImmediate(() => autoUpdater.quitAndInstall())
+  })
+})
+
+// export this to MenuItem click callback
+function checkForUpdates (menuItem, focusedWindow, event) {
+  updater = menuItem
+  updater.enabled = false
+  autoUpdater.checkForUpdates()
+}
